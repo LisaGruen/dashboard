afd814ab34c79ba31696d7146d279ee9a0c38c76
indre101 <zygaityte.indre@gmail.com>
2020-05-12 20:13:37 +0200

    adds basic data to states

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import DataBase from "./modules/DataBase";
+
 import "./App.scss";
 
 function App() {
+  const [barInformation, setBarInformation] = useState({});
+  const [bartenders, setBartenders] = useState([]);
+  const [queue, setQueue] = useState([]);
+  const [serving, setServing] = useState([]);
+  const [storage, setStorage] = useState([]);
+  const [taps, setTaps] = useState([]);
+
+  const setInitalData = async () => {
+    const information = await DataBase();
+    console.log(information);
+
+    setBarInformation(information.bar);
+    setBartenders(information.bartenders);
+    setQueue(information.queue);
+    setServing(information.serving);
+    setStorage(information.storage);
+    setTaps(information.taps);
+  };
+
+  useEffect(() => {
+    setInitalData();
+    const getDataIntervalID = setInterval(() => {
+      setInitalData();
+    }, 5000);
+
+    return () => clearInterval(getDataIntervalID);
+  }, []);
+
   return (
     <div className="App">
       <h1>hi</h1>
---
+++ src/modules/DataBase.js
@@ -0,0 +1,10 @@
+const Data = async () => {
+  const beerData = await fetch(
+    "https://more-beers-less-tears-data.herokuapp.com/"
+  );
+
+  const response = await beerData.json();
+  return response;
+};
+
+export default Data;
