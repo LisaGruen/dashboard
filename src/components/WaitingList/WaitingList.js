89a1839b3df27c8fa5d56a48ab60f641357b5fca
your actual name <your email address used to login at githubtonevadarina@gmail.com>
2020-05-21 09:14:06 +0200

    initial styling for waiting list & serving section done

--- src/components/WaitingList/WaitingList.js
+++ src/components/WaitingList/WaitingList.js
@@ -5,21 +5,33 @@ export default function WaitingList(props) {
   console.log(props.currentQueue);
   return (
     <div className="WRAPPER">
+      <h2>BEER IS READY!</h2>{" "}
+      <div className="container">
+        {props.currentServing.map(({ id }) => (
+          <div className="serving " style={{ color: "lightgreen" }} key={id}>
+            {" "}
+            {id}{" "}
+          </div>
+        ))}{" "}
+      </div>
       <h2>
         ALMOST THERE <br /> DO NOT GIVE UP!
       </h2>
-      {props.currentServing.map(({ id }) => (
-        <div className="orderNumber" style={{ color: "red" }} key={id}>
-          {id}
-        </div>
-      ))}
-      {props.currentQueue.map(({ id, startTime }) => (
-        <div className="orderNumber" key={id}>
-          {id}
-        </div>
-      ))}
-
-      <div className="numbersContainer"></div>
+      <div className="container">
+        {props.currentQueue.map(({ id, startTime }) => (
+          <div className="queue" style={{ color: "red" }} key={id}>
+            {" "}
+            {id}{" "}
+          </div>
+        ))}
+      </div>
+      <div>
+        <img
+          className="cheersImg"
+          src={require("../../assets/images/graphics/orderReady.svg")}
+          alt="Cheers!"
+        />
+      </div>
     </div>
   );
 }
