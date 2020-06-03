311e320d9f01f2d13e2d92121f1ad8242af8df4c
indre101 <zygaityte.indre@gmail.com>
2020-06-03 19:02:40 +0200

    fixes position

--- src/components/Announcement/Announcement.js
+++ src/components/Announcement/Announcement.js
@@ -9,7 +9,7 @@ class Announcement extends React.Component {
         <div className="wrapperInside">
           <h3>Every Friday 6 - 7 PM</h3>
           <h2>HAPPY HOUR</h2>
-          <h3>Buy a beer and get one for free</h3>
+          <h3 className="annoucmentDeal">Buy a beer and get one for free</h3>
         </div>
       </div>
     );
